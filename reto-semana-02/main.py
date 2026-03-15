69798f1a8f61bc073db1aaf22c2ee7a9b4d56483
Roony-6 <roony7.rrc@gmail.com>
2026-03-14 19:43:34 -0600

    Agregar main

--- reto-semana-02/main.py
+++ reto-semana-02/main.py
@@ -1,12 +1,12 @@
 import sys
 
 def fahrenheit_a_celsius(fahrenheit:float) -> float: 
-    """Convierte Fahrenheit a Celsius."""
+    """ Convierte Fahrenheit a Celsius."""
     return (fahrenheit - 32) * 5 / 9
 
 
 def clasificar_temperatura(celsius: float) -> float:
-    """Clasifica la temperatura segun rangos definidos."""
+    """ Clasifica la temperatura segun rangos definidos."""
     if celsius < 0:
         return "Congelante"
     elif celsius <= 15:  # 0 a 15
@@ -19,5 +19,43 @@ def clasificar_temperatura(celsius: float) -> float:
         return "Extremo"
     
 def procesar_linea(linea:str) -> str:
-    pass
+    """ Procesa cada linea de la entrada stdin"""
+
+    if not linea:
+        return False
+    
+    
+    elementos= linea.strip().split()
+    
+    if (len(elementos) != 3):
+        return False
+    if elementos[2].upper() not in ["C","F"]:
+        return False
+    
+        
+    try:
+                
+        ciudad = elementos[0].upper()
+        grados = f"{float(elementos[1].strip()):.2f}"
+        clasificacion = clasificar_temperatura(fahrenheit_a_celsius(elementos[2]) if elementos[2] == "F" else elementos[2].upper())
+        
+    except :
+        return False
+    
+    return f"{ciudad},{grados},{clasificacion}"
+    
+    
+
+def main():
+    #imprimir encabezado
+    print("ciudad,temperatura_celsius,clasificacion")
+    
+    for linea in sys.stdin:
+        continue
+    
+    
+if __name__ == "__main__":
+    main()
+        
+
     
